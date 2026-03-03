0d28641244f715204103794be7b0909b8cb9628a
Mohit Singh <mohitchauhan6585@gmail.com>
2026-03-04 00:31:04 +0530

    build fix

--- client/src/navbar.tsx
+++ client/src/navbar.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet" // For Mobile
-import { Menu} from "lucide-react" // Icons
+import { Menu} from "lucide-react" 
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 import { Link } from "react-router-dom"
 
